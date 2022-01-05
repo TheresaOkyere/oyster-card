b4a812acbbaa1b88975a1bc8767f34e5ea280dd8
code-to <Theresaokyere034@gmail.com>
2022-01-05 17:06:18 +0000

    deduct,in_journey and minimum balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,13 +3,34 @@ class Oystercard
 
 MAXIMUM_BALANCE = 90 
 
+
   def initialize
     @balance = 0
+    @journey_status = false
   end
 
   def top_up(num)
     fail 'You have reached your balance limit' if num + balance > MAXIMUM_BALANCE
     @balance += num
   end
+
+  def deduct(fare)
+    @balance -= fare
+  end
+
+  def in_journey?
+    @journey_status
+  end
+
+def touch_in
+  fail 'insufficent balance' if @balance < 1 
+  @journey_status = true
+  
+end
+
+def touch_out
+  @journey_status = false
+end
+
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,9 +6,9 @@ describe Oystercard do
     expect(subject.balance).to eq 0
   end
 
-it 'allow user to add balance' do
-  oyster = Oystercard.new
-  expect(oyster).to respond_to(:top_up).with(1).argument
+ it 'allow user to add balance' do
+   oyster = Oystercard.new
+   expect(oyster).to respond_to(:top_up).with(1).argument
 end
 
 it 'updates the balance' do
@@ -22,4 +22,51 @@ it 'has a balance limit' do
   expect{ subject.top_up 1 }.to raise_error 'You have reached your balance limit'
 end
 
+ it 'deduct fare from balance' do
+  subject.top_up(21)
+  expect{subject.deduct 3 }.to change{subject.balance}.by -3
+ end
+
+describe '#in_journey' do
+it { is_expected.to respond_to(:in_journey?) }
+
+it 'checks if user had touch in' do
+  subject.top_up(5)
+  subject.touch_in
+  expect(subject.in_journey?).to eq true
 end
+
+it 'checks if user had touch out' do
+  expect(subject.in_journey?).to eq false
+end
+end
+
+
+describe '#touch_in' do
+it {is_expected.to respond_to(:touch_in)}
+
+it 'allows user to touch in' do
+  subject.top_up(5)
+  subject.touch_in
+  expect(subject).to be_in_journey
+end
+
+it 'will not touch in if below minimum balance' do
+  expect{ subject.touch_in }.to raise_error "Insufficient balance"
+end
+end
+
+describe '#touch_out' do
+it {is_expected.to respond_to(:touch_out)}
+
+it 'allows the user to touch out' do
+  subject.top_up(5)
+  subject.touch_in
+  subject.touch_out
+  expect(subject).not_to be_in_journey
+end
+end
+
+
+end
+
